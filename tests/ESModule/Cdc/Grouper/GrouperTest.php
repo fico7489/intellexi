ba5422827ca63a221bb69140341d2cb55db8c88d
Filip Horvat <fico7489@gmail.com>
2024-12-01 22:24:31 +0100

    22

---
+++ tests/ESModule/Cdc/Grouper/GrouperTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\ESModule\Cdc\Grouper;
+
+use Tests\TestCase;
+
+class GrouperTest extends TestCase
+{
+    public function testProcessor()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
