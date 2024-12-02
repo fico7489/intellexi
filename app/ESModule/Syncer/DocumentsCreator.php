dbdcb4f5fd78c9cde97333ff20044d21ab4bef71
Filip Horvat <fico7489@gmail.com>
2024-12-02 14:29:27 +0100

    1234

--- app/ESModule/Syncer/DocumentsCreator.php
+++ app/ESModule/Syncer/DocumentsCreator.php
@@ -5,18 +5,21 @@ namespace App\ESModule\Syncer;
 use App\ESModule\Cdc\Dto\ChangedRowDto;
 use App\ESModule\Cdc\Dto\ChangedRowGroupedDto;
 use App\ESModule\Syncer\Dto\Document;
-use App\Models\Application;
-use App\Models\User;
 
 class DocumentsCreator
 {
+    public function __construct(
+        private readonly ModelsDetector $modelsDetector,
+    ) {
+    }
+
     public function createDocuments($changedRowsGrouped): array
     {
         $documents = [];
         foreach ($changedRowsGrouped as $table => $data) {
             foreach ($data as $identifier => $changedRowGrouped) {
                 /* @var ChangedRowGroupedDto $changedRowGrouped */
-                $models = $this->detectModels($changedRowGrouped);
+                $models = $this->modelsDetector->detectModels($changedRowGrouped);
 
                 foreach ($models as $indexName => $model) {
                     $documents[$indexName][] = new Document(
@@ -30,19 +33,5 @@ class DocumentsCreator
         }
 
         return $documents;
-    }
-
-    private function detectModels(ChangedRowGroupedDto $changedRowGrouped): array
-    {
-        $className = 'applications' === $changedRowGrouped->getTable() ? Application::class : User::class;
-        $indexName = 'applications' === $changedRowGrouped->getTable() ? 'prefix_applications' : 'prefix_users';
-
-        $model = $className::find($changedRowGrouped->getIdentifier());
-
-        $models = [
-            $indexName => $model,
-        ];
-
-        return $models;
     }
 }
--- app/ESModule/Syncer/Syncer.php
+++ app/ESModule/Syncer/Syncer.php
@@ -7,7 +7,7 @@ use App\ESModule\Cdc\Event\CdcChangedRowsGrouped;
 class Syncer
 {
     public function __construct(
-        private readonly DocumentsCreator     $documentsCreator,
+        private readonly DocumentsCreator $documentsCreator,
         private readonly SearchEngineEsSyncer $searchEngineSyncer,// TODO by interface
     ) {
     }
