bf6c7cfeb91381a1da971602636a206b3bdca5b8
Filip Horvat <fico7489@gmail.com>
2024-12-05 17:33:43 +0100

    12

---
+++ tests/ESModule/Syncer/Creator/Sync/Flow/IsTableNameForSyncTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\ESModule\Syncer\Creator\Sync\Flow;
+
+use App\ESModule\Syncer\Mapper\DatabaseMapper\DatabaseMapper;
+use App\ESModule\Syncer\Mapper\SyncMapper\SyncMapper;
+use Mockery\MockInterface;
+use Tests\ESModule\Syncer\Creator\Sync\TestCase;
+
+class IsTableNameForSyncTest extends TestCase
+{
+    public function testIsForSync()
+    {
+        $cdcDto = $this->createCdcDto();
+        $cdcDtos = [$cdcDto];
+
+        $this->mock(SyncMapper::class, function (MockInterface $mock) {
+            $mock->allows('isDatabaseNameForSync')->andReturn(true);
+            $mock->allows('isTableNameForSync')->andReturn(true);
+        })->makePartial();
+
+        $data = $this->createService()->create($cdcDtos);
+        $this->assertEquals(1, count($data));
+    }
+
+    public function testIsNotForSync()
+    {
+        $cdcDto = $this->createCdcDto();
+        $cdcDtos = [$cdcDto];
+
+        $this->mock(SyncMapper::class, function (MockInterface $mock) {
+            $mock->allows('isDatabaseNameForSync')->andReturn(true);
+            $mock->allows('isTableNameForSync')->andReturn(false);
+        })->makePartial();
+
+        $data = $this->createService()->create($cdcDtos);
+        $this->assertEquals(0, count($data));
+    }
+}
