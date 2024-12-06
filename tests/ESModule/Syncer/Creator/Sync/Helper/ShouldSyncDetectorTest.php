c6c8d57d30ee429064ad4b3ae1b73f4ed532eb90
Filip Horvat <fico7489@gmail.com>
2024-12-06 09:28:31 +0100

    12

---
+++ tests/ESModule/Syncer/Creator/Sync/Helper/ShouldSyncDetectorTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\ESModule\Syncer\Creator\Sync\Helper;
+
+use App\ESModule\Syncer\Creator\Document\Helper\ShouldSyncDetector;
+use Tests\ESModule\Syncer\Creator\Sync\TestCase;
+
+class ShouldSyncDetectorTest extends TestCase
+{
+    public function testIsForSync()
+    {
+        $this->assertEquals(true, app(ShouldSyncDetector::class)->detect(['id'], ['id', 'name']));
+    }
+
+    public function testIsForSync2()
+    {
+        $this->assertEquals(true, app(ShouldSyncDetector::class)->detect(['id', 'name'], ['id', 'name']));
+    }
+
+    public function testIsForSync3()
+    {
+        $this->assertEquals(true, app(ShouldSyncDetector::class)->detect(['id', 'name', 'test'], ['id', 'name']));
+    }
+
+    public function testIsNotForSync()
+    {
+        $this->assertEquals(false, app(ShouldSyncDetector::class)->detect([], ['id', 'name']));
+    }
+
+    public function testIsNotForSync2()
+    {
+
+        $this->assertEquals(false, app(ShouldSyncDetector::class)->detect(['id2'], ['id', 'name']));
+    }
+
+    public function testIsNotForSync3()
+    {
+        $this->assertEquals(false, app(ShouldSyncDetector::class)->detect(['id2', 'name2'], ['id', 'name']));
+    }
+
+    public function testIsNotForSync4()
+    {
+        $this->assertEquals(false, app(ShouldSyncDetector::class)->detect(['id2', 'name2', 'test2'],  ['id', 'name']));
+    }
+}
