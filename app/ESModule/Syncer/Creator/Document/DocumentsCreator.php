052eca19a61b7ca417b88c3bde5704a03a288180
Filip Horvat <fico7489@gmail.com>
2024-12-06 11:17:39 +0100

    1234

--- app/ESModule/Syncer/Creator/Document/DocumentsCreator.php
+++ app/ESModule/Syncer/Creator/Document/DocumentsCreator.php
@@ -2,28 +2,20 @@
 
 namespace App\ESModule\Syncer\Creator\Document;
 
-use App\ESModule\Config\Interface\IndexDefinerModelInterface;
 use App\ESModule\Syncer\Creator\Document\Dto\DocumentDto;
-use App\ESModule\Syncer\Creator\Document\Helper\ModelSourceFetcher;
-use App\ESModule\Syncer\Creator\Document\Helper\ModelsRelatedFetcher;
-use App\ESModule\Syncer\Creator\Document\Helper\ModelsRelatedValidatorAndGrouper;
 use App\ESModule\Syncer\Creator\Document\Helper\ShouldSyncDetector;
+use App\ESModule\Syncer\Creator\Document\Helper\SyncItemDocumentCreator;
 use App\ESModule\Syncer\Creator\SyncItem\Dto\SyncItemDto;
-use App\ESModule\Syncer\Fetcher\DataFetcher;
 use App\ESModule\Syncer\Mapper\IndexMapper\IndexMapper;
 use App\ESModule\Syncer\Mapper\SyncMapper\SyncMapper;
-use Illuminate\Database\Eloquent\Model;
 
 class DocumentsCreator
 {
     public function __construct(
-        private readonly DataFetcher $dataFetcher,
         private readonly SyncMapper $syncMapper,
         private readonly IndexMapper $indexMapper,
-        private readonly ModelSourceFetcher $modelSourceFetcher,
-        private readonly ModelsRelatedFetcher $modelsRelatedFetcher,
         private readonly ShouldSyncDetector $shouldSyncDetector,
-        private readonly ModelsRelatedValidatorAndGrouper $modelsRelatedValidatorAndGrouper,
+        private readonly SyncItemDocumentCreator $syncItemDocumentCreator,
     ) {
     }
 
@@ -59,48 +51,15 @@ class DocumentsCreator
         foreach ($syncMapping as $tableName => $indexData) {
             foreach ($indexData as $indexName => $changedFieldsTriggers) {
                 // sync is matched by changed table $syncItemDto and table from $syncMapping
-                if ($tableName === $syncItemDto->getTableName()) {
-                    if ($this->shouldSyncDetector->detect($syncItemDto->getChangedFields(), $changedFieldsTriggers)) {
-                        $index = $this->indexMapper->fetchIndexByIndexName($indexName);
-                        $className = $index->getClassName();
+                if ($tableName === $syncItemDto->getTableName()
+                    && $this->shouldSyncDetector->detect($syncItemDto->getChangedFields(), $changedFieldsTriggers)
+                ) {
+                    $index = $this->indexMapper->fetchIndexByIndexName($indexName);
+                    $className = $index->getClassName();
 
-                        $modelSource = $this->modelSourceFetcher->fetch($syncItemDto);
-                        $modelsRelated = $this->modelsRelatedFetcher->fetch($syncItemDto, $index, $modelSource);
-                        $modelsRelated = $this->modelsRelatedValidatorAndGrouper->validateAndGroup($modelsRelated, $className);
-                        $documents = array_merge($documents, $this->createDocumentsForModelsRelated($syncItemDto, $index, $modelsRelated, $modelSource));
-                    }
+                    $documents = array_merge($documents, $this->syncItemDocumentCreator->create($syncItemDto, $index));
                 }
             }
-        }
-
-        return $documents;
-    }
-
-    private function createDocumentsForModelsRelated(
-        SyncItemDto $syncItemDto,
-        IndexDefinerModelInterface $index,
-        array $modelsRelated,
-        object $modelSource,
-    ): array {
-        // TODO make updates unique by model->id
-
-        $documents = [];
-        foreach ($modelsRelated as $modelRelated) {
-            /** @var Model $modelRelated */
-
-            // TODO prefix
-            $indexName = 'prefix_'.$index->getIndexName();
-
-            $identifierName = $modelRelated->getKeyName();
-            $identifierValue = $modelRelated->{$identifierName};
-            $data = $this->dataFetcher->fetch($index, $modelRelated);
-
-            $type = DocumentDto::TYPE_UPSERT;
-            if ($modelRelated === $modelSource) {
-                $type = $syncItemDto->getType();
-            }
-
-            $documents[] = new DocumentDto($indexName, $identifierValue, $data, $type);
         }
 
         return $documents;
---
+++ app/ESModule/Syncer/Creator/Document/Helper/SyncItemDocumentCreator.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\ESModule\Syncer\Creator\Document\Helper;
+
+use App\ESModule\Config\Interface\IndexDefinerModelInterface;
+use App\ESModule\Syncer\Creator\Document\Dto\DocumentDto;
+use App\ESModule\Syncer\Creator\SyncItem\Dto\SyncItemDto;
+use App\ESModule\Syncer\Fetcher\DataFetcher;
+use Illuminate\Database\Eloquent\Model;
+
+class SyncItemDocumentCreator
+{
+    public function __construct(
+        private readonly ModelSourceFetcher $modelSourceFetcher,
+        private readonly ModelsRelatedFetcher $modelsRelatedFetcher,
+        private readonly ModelsRelatedValidatorAndGrouper $modelsRelatedValidatorAndGrouper,
+        private readonly DataFetcher $dataFetcher,
+    ) {
+    }
+
+    public function create(SyncItemDto $syncItemDto, IndexDefinerModelInterface $index): array
+    {
+        $className = $index->getClassName();
+
+        $modelSource = $this->modelSourceFetcher->fetch($syncItemDto);
+        $modelsRelated = $this->modelsRelatedFetcher->fetch($syncItemDto, $index, $modelSource);
+        $modelsRelated = $this->modelsRelatedValidatorAndGrouper->validateAndGroup($modelsRelated, $className);
+
+        $documents = $this->createDocumentsForModelsRelated($syncItemDto, $index, $modelsRelated, $modelSource);
+
+        return $documents;
+    }
+
+    private function createDocumentsForModelsRelated(
+        SyncItemDto $syncItemDto,
+        IndexDefinerModelInterface $index,
+        array $modelsRelated,
+        object $modelSource,
+    ): array {
+        // TODO make updates unique by model->id
+
+        $documents = [];
+        foreach ($modelsRelated as $modelRelated) {
+            /** @var Model $modelRelated */
+
+            // TODO prefix
+            $indexName = 'prefix_'.$index->getIndexName();
+
+            $identifierName = $modelRelated->getKeyName();
+            $identifierValue = $modelRelated->{$identifierName};
+            $data = $this->dataFetcher->fetch($index, $modelRelated);
+
+            $type = DocumentDto::TYPE_UPSERT;
+            if ($modelRelated === $modelSource) {
+                $type = $syncItemDto->getType();
+            }
+
+            $documents[] = new DocumentDto($indexName, $identifierValue, $data, $type);
+        }
+
+        return $documents;
+    }
+}
