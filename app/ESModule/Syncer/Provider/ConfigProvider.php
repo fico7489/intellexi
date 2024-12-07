0e3c7a830e2e1c55df46437557c49e4ad8336ad8
Filip Horvat <fico7489@gmail.com>
2024-12-07 10:49:44 +0100

    1234

--- app/ESModule/Syncer/Provider/ConfigProvider.php
+++ app/ESModule/Syncer/Provider/ConfigProvider.php
@@ -4,7 +4,6 @@ namespace App\ESModule\Syncer\Provider;
 
 use App\ES\Connection\DefaultConnection;
 use App\ESModule\Config\Interface\IndexModelInterface;
-use App\ESModule\Syncer\Adapter\OrmAdapter\OrmAdapter;
 use App\ESModule\Syncer\Provider\Builder\ConfigDtoBuilder;
 use App\ESModule\Syncer\Provider\Builder\Dto\ConfigDto;
 use App\ESModule\Syncer\Provider\Builder\Dto\ConnectionDto;
@@ -18,7 +17,6 @@ class ConfigProvider
         // TODO
         private readonly DefaultConnection $configConnection,
         private readonly array $configIndexes,
-        private readonly OrmAdapter $ormAdapter,
         private readonly ConfigDtoBuilder $configDtoBuilder,
     ) {
     }
@@ -43,25 +41,8 @@ class ConfigProvider
         return isset($tableNamesIndex[$tableName]);
     }
 
-    /**
-     * @return array<IndexModelInterface>
-     */
-    public function fetchClassNamesOrmToIndexMapping(): array
-    {
-        $indexDefiners = $this->configIndexes;
-
-        $classNamesOrmToIndexMapping = [];
-        foreach ($indexDefiners as $indexDefiner) {
-            $classNamesOrmToIndexMapping[$indexDefiner->getClassNameOrm()] = $indexDefiner;
-        }
-
-        return $classNamesOrmToIndexMapping;
-    }
-
     public function fetchIndexByIndexName(string $indexName): IndexModelInterface
     {
-        $classNamesOrmToIndexMapping = $this->fetchClassNamesOrmToIndexMapping();
-
         $indexDefiners = $this->configIndexes;
         foreach ($indexDefiners as $indexDefiner) {
             if ($indexDefiner->getIndexName() === $indexName) {
