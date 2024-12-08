7a9802f065319c2e6a79b4d1a2a77837f4dd8e05
Filip Horvat <fico7489@gmail.com>
2024-12-08 12:25:34 +0100

    1234

--- app/ESModule/Syncer/Creator/Document/DocumentsCreator.php
+++ app/ESModule/Syncer/Creator/Document/DocumentsCreator.php
@@ -2,9 +2,11 @@
 
 namespace App\ESModule\Syncer\Creator\Document;
 
+use App\ESModule\Syncer\Creator\Document\DocumentsCreator\DocumentCreator;
 use App\ESModule\Syncer\Creator\Document\DocumentsCreator\DocumentsGrouper;
-use App\ESModule\Syncer\Creator\Document\DocumentsCreator\MatchedSyncItemCreator;
 use App\ESModule\Syncer\Creator\Document\Dto\DocumentDto;
+use App\ESModule\Syncer\Creator\Document\Helper\ModelSourceFetcher;
+use App\ESModule\Syncer\Creator\Document\Helper\ShouldSyncDetector;
 use App\ESModule\Syncer\Creator\SyncItem\Dto\SyncItemDto;
 use App\ESModule\Syncer\Provider\ConfigProvider;
 
@@ -12,8 +14,10 @@ class DocumentsCreator
 {
     public function __construct(
         private readonly ConfigProvider $configProvider,
-        private readonly MatchedSyncItemCreator $matchedSyncItemCreator,
         private readonly DocumentsGrouper $documentsGrouper,
+        private readonly ShouldSyncDetector $shouldSyncDetector,
+        private readonly DocumentCreator $documentCreator,
+        private readonly ModelSourceFetcher $modelSourceFetcher,
     ) {
     }
 
@@ -24,13 +28,16 @@ class DocumentsCreator
      */
     public function create(array $syncItemDtos): array
     {
+        dump('count $syncItemDtos='.count($syncItemDtos));
+
         $syncMapping = $this->configProvider->getConfigDto()->getSyncMap();
 
         $documents = [];
         foreach ($syncItemDtos as $syncItemDto) {
             foreach ($syncMapping as $tableName => $indexData) {
                 foreach ($indexData as $indexName => $changedFieldsTriggers) {
-                    $documents = array_merge($documents, $this->matchedSyncItemCreator->createForMatched($syncItemDto, $tableName, $indexName, $changedFieldsTriggers));
+                    dump(2222);
+                    $documents = array_merge($documents, $this->createForMatched($syncItemDto, $tableName, $indexName, $changedFieldsTriggers));
                 }
             }
         }
@@ -39,5 +46,24 @@ class DocumentsCreator
 
         // return  $documents;
         return $this->documentsGrouper->group($documents);
+    }
+
+    private function createForMatched($syncItemDto, $tableName, $indexName, $changedFieldsTriggers): array
+    {
+        if ($tableName !== $syncItemDto->getTableName()) {
+            return [];
+        }
+
+        // sync is matched by changed table $syncItemDto and table from $syncMapping
+        if (!$this->shouldSyncDetector->detect($syncItemDto->getChangedFields(), $changedFieldsTriggers)) {
+            return [];
+        }
+
+        $index = $this->configProvider->fetchIndexByIndexName($indexName);
+
+        $modelSource = $this->modelSourceFetcher->fetch($syncItemDto);
+        // dump($syncItemDto->getTableName(), $syncItemDto->getIdentifierValue(), $syncItemDto->getChangedFields(), is_null($modelSource));
+
+        return $this->documentCreator->create($syncItemDto, $index, $modelSource);
     }
 }
