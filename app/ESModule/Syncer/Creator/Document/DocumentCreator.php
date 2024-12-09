d57dd71a32cd4280c8f1d92854b22a5aba99a23e
Filip Horvat <fico7489@gmail.com>
2024-12-09 15:21:28 +0100

    1234

--- app/ESModule/Syncer/Creator/Document/DocumentCreator.php
+++ app/ESModule/Syncer/Creator/Document/DocumentCreator.php
@@ -4,8 +4,7 @@ namespace App\ESModule\Syncer\Creator\Document;
 
 use App\ESModule\Syncer\Adapter\OrmAdapter\OrmAdapter;
 use App\ESModule\Syncer\Creator\CdcSyncable\Dto\CdcSyncableDto;
-use App\ESModule\Syncer\Creator\Document\Dto\DocumentDto;
-use App\ESModule\Syncer\Creator\Document\Helper\ModelsRelatedFetcher;
+use App\ESModule\Syncer\Creator\Document\Helper\ModelMapCreator;
 use App\ESModule\Syncer\Provider\ConfigProvider;
 
 class DocumentCreator
@@ -13,8 +12,8 @@ class DocumentCreator
     public function __construct(
         private readonly ConfigProvider $configProvider,
         private readonly OrmAdapter $ormAdapter,
-        private readonly ModelsRelatedFetcher $modelsRelatedFetcher,
         private readonly EsDocumentsCreator $documentsCreator,
+        private readonly ModelMapCreator $modelMapCreator,
     ) {
     }
 
@@ -23,57 +22,27 @@ class DocumentCreator
      */
     public function create(array $syncItemDtos): array
     {
-        $documents = [];
+        $modelMapDtos = [];
         foreach ($syncItemDtos as $syncItemDto) {
             $indexNames = $syncItemDto->getIndexNamesForSync();
             foreach ($indexNames as $indexName) {
                 // detect $modelSource
                 $modelSource = $this->fetchModelSource($syncItemDto);
 
-                $documents = $this->createDocuments($documents, $syncItemDto, $modelSource, $indexName);
+                $modelMapDtos = $this->modelMapCreator->createModelMapDtosForIndex($modelMapDtos, $syncItemDto, $modelSource, $indexName);
             }
         }
 
-        $documentsFlatten = [];
-        foreach ($documents as $tableNameRelated => $data) {
+        $modelMapDtosFlattened = [];
+        foreach ($modelMapDtos as $tableNameRelated => $data) {
             foreach ($data as $identifierValue => $dto) {
-                $documentsFlatten[] = $dto;
+                $modelMapDtosFlattened[] = $dto;
             }
         }
 
-        $documents = $this->documentsCreator->create($documentsFlatten);
+        $modelMapDtos = $this->documentsCreator->create($modelMapDtosFlattened);
 
-        return $documents;
-    }
-
-    private function createDocuments(array $syncModels, CdcSyncableDto $syncItemDto, $modelSource, $indexNameRelated): array
-    {
-        $tableNameRelated = $this->configProvider->fetchTableNameByIndexName($indexNameRelated);
-
-        // detect $indexDto
-        $indexDtoRelated = $this->configProvider->fetchIndexDtoByIndexName($indexNameRelated);
-
-        $modelsRelated = $this->modelsRelatedFetcher->fetch($syncItemDto, $indexDtoRelated, $modelSource);
-
-        // TODO make updates unique by model->id
-
-        foreach ($modelsRelated as $modelRelated) {
-            $type = $syncItemDto->getType();
-            if ($modelRelated !== $modelSource) {
-                $type = DocumentDto::TYPE_UPSERT;
-            }
-
-            $identifierName = $modelRelated->getKeyName();
-            $identifierValue = $modelRelated->{$identifierName};
-
-            $syncModels[$tableNameRelated][$identifierValue] = [
-                'indexName' => $indexNameRelated,
-                'identifierValue' => $identifierValue,
-                'type' => $type,
-            ];
-        }
-
-        return $syncModels;
+        return $modelMapDtos;
     }
 
     private function fetchModelSource(CdcSyncableDto $syncItemDto): ?object
---
+++ app/ESModule/Syncer/Creator/Document/Helper/ModelMapCreator.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\ESModule\Syncer\Creator\Document\Helper;
+
+use App\ESModule\Syncer\Creator\CdcSyncable\Dto\CdcSyncableDto;
+use App\ESModule\Syncer\Creator\Document\Dto\DocumentDto;
+use App\ESModule\Syncer\Provider\ConfigProvider;
+
+class ModelMapCreator
+{
+    public function __construct(
+        private readonly ConfigProvider $configProvider,
+        private readonly ModelsRelatedFetcher $modelsRelatedFetcher,
+    ) {
+    }
+
+    public function createModelMapDtosForIndex(array $modelMapDtos, CdcSyncableDto $syncItemDto, $modelSource, $indexNameRelated): array
+    {
+        $tableNameRelated = $this->configProvider->fetchTableNameByIndexName($indexNameRelated);
+
+        // detect $indexDto
+        $indexDtoRelated = $this->configProvider->fetchIndexDtoByIndexName($indexNameRelated);
+
+        $modelsRelated = $this->modelsRelatedFetcher->fetch($syncItemDto, $indexDtoRelated, $modelSource);
+
+        foreach ($modelsRelated as $modelRelated) {
+            $type = $syncItemDto->getType();
+            if ($modelRelated !== $modelSource) {
+                $type = DocumentDto::TYPE_UPSERT;
+            }
+
+            $identifierName = $modelRelated->getKeyName();
+            $identifierValue = $modelRelated->{$identifierName};
+
+            $modelMapDtos[$tableNameRelated][$identifierValue] = [
+                'indexName' => $indexNameRelated,
+                'identifierValue' => $identifierValue,
+                'type' => $type,
+            ];
+        }
+
+        return $modelMapDtos;
+    }
+}
