e5a15dd8e6208eaceee823d706a9d915bc938354
Filip Horvat <fico7489@gmail.com>
2024-12-10 09:21:17 +0100

    add test

--- tests/ESModule/Syncer/Creator/Document/DocumentCreatorTest.php
+++ tests/ESModule/Syncer/Creator/Document/DocumentCreatorTest.php
@@ -2,13 +2,36 @@
 
 namespace Tests\ESModule\Syncer\Creator\Document;
 
+use App\ESModule\Syncer\Creator\CdcSyncable\Dto\CdcSyncableDto;
+use App\ESModule\Syncer\Creator\Document\Converter\IndexModelsToDocumentsConverter;
 use App\ESModule\Syncer\Creator\Document\DocumentCreator;
+use App\ESModule\Syncer\Creator\Document\Dto\DocumentDto;
+use App\ESModule\Syncer\Creator\Document\Dto\IndexModelDto;
+use App\ESModule\Syncer\Creator\Document\IndexModel\IndexModelCreator;
 use Tests\TestCase;
 
 class DocumentCreatorTest extends TestCase
 {
     public function testService()
     {
-        $documentDtos = app(DocumentCreator::class)->create();
+        $cdcSyncableDto = new CdcSyncableDto('test-table', CdcSyncableDto::TYPE_UPSERT, [], [], 1, []);
+        $indexModelDto = new IndexModelDto('test-index', 1, CdcSyncableDto::TYPE_UPSERT);
+        $documentDto = new DocumentDto('test-index', 1, [], DocumentDto::TYPE_UPSERT);
+
+        $this->mock(IndexModelCreator::class, function ($mock) use ($indexModelDto, $cdcSyncableDto) {
+            $mock->shouldReceive('create')
+                ->with([$cdcSyncableDto])
+                ->andReturn([$indexModelDto])
+                ->once();
+        });
+
+        $this->mock(IndexModelsToDocumentsConverter::class, function ($mock) use ($indexModelDto, $documentDto) {
+            $mock->shouldReceive('convert')
+                ->with([$indexModelDto])
+                ->andReturn([$documentDto])
+                ->once();
+        });
+
+        $documentDtos = app(DocumentCreator::class)->create([$cdcSyncableDto]);
     }
 }
